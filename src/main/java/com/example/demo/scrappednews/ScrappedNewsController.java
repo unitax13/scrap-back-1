42a04affc73adb360295c1b409a396920d6ee827
unitax13 <61455951+unitax13@users.noreply.github.com>
2023-01-08 23:16:06 +0100

    added unavailability warnings system

--- src/main/java/com/example/demo/scrappednews/ScrappedNewsController.java
+++ src/main/java/com/example/demo/scrappednews/ScrappedNewsController.java
@@ -1,7 +1,6 @@
 package com.example.demo.scrappednews;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
@@ -17,9 +16,8 @@ import java.util.*;
 @RequestMapping(path = "news/get")
 public class ScrappedNewsController {
 
-   private final ScrappedNewsService newsService;
    final ArrayList<String> services = new ArrayList<>(Arrays.asList("dgp", "polsatnews", "rmf24", "tvn24", "gazetapl", "wydarzeniainteria"));
-
+   private final ScrappedNewsService newsService;
    ArrayList<ScrappedNewsDGP> newsDGP = new ArrayList<>();
    ArrayList<ScrappedNewsGazetaPl> newsGazetaPl = new ArrayList<>();
    ArrayList<ScrappedNewsPolsatNews> newsPolsatNews = new ArrayList<>();
@@ -35,7 +33,9 @@ public class ScrappedNewsController {
    TreeMap<LocalDate, Integer> gazetaplTimeTable = new TreeMap<>();
    TreeMap<LocalDate, Integer> wydarzeniainteriaTimeTable = new TreeMap<>();
 
-   HashMap<String,TreeMap<LocalDate,Integer>> timetables = new HashMap<>();
+   HashMap<String, TreeMap<LocalDate, Integer>> timetables = new HashMap<>();
+   //HashMap<String, ArrayList<String>> textWarnings = new HashMap<>();
+
 
    LocalTime dbFetchTime;
    LocalTime nextFetchTime;
@@ -45,7 +45,8 @@ public class ScrappedNewsController {
       this.newsService = newsService;
       RefetchAspect.snc = this;
       fetchDataFromRepositories();
-      generateTimeTables();
+      timetables = generateTimeTables(null);
+//      generateTextWarnings();
    }
 
    private void fetchDataFromRepositories() {
@@ -69,53 +70,63 @@ public class ScrappedNewsController {
    }
 
    public void checkIfItsTimeToRefetchAndIfSoDoSo() {
-      if (LocalTime.now().isAfter(nextFetchTime)) {
-         System.out.println("It's time for a new fetch.");
-         fetchDataFromRepositories();
-      }
+//      if (LocalTime.now().isAfter(nextFetchTime)) {
+//         System.out.println("It's time for a new fetch.");
+//         fetchDataFromRepositories();
+//      }
    }
 
-   private void generateTimeTables() {
-      dgpTimeTable = generateTimeTable("dgp");
-      polsatnewsTimeTable = generateTimeTable("polsatnews");
-      rmf24TimeTable = generateTimeTable("rmf24");
-      tvn24TimeTable = generateTimeTable("tvn24");
-      gazetaplTimeTable = generateTimeTable("gazetapl");
-      wydarzeniainteriaTimeTable = generateTimeTable("wydarzeniainteria");
+   private HashMap<String, TreeMap<LocalDate, Integer>> generateTimeTables(ArrayList<ScrappedNews> newsPool) {
+      dgpTimeTable = generateTimeTable("dgp", newsPool);
+      polsatnewsTimeTable = generateTimeTable("polsatnews", newsPool);
+      rmf24TimeTable = generateTimeTable("rmf24", newsPool);
+      tvn24TimeTable = generateTimeTable("tvn24", newsPool);
+      gazetaplTimeTable = generateTimeTable("gazetapl", newsPool);
+      wydarzeniainteriaTimeTable = generateTimeTable("wydarzeniainteria", newsPool);
 
-      timetables.put(ScrappedNewsDGP.getStringRepresentation(),dgpTimeTable);
-      timetables.put(ScrappedNewsPolsatNews.getStringRepresentation(),polsatnewsTimeTable);
-      timetables.put(ScrappedNewsRMF24.getStringRepresentation(),rmf24TimeTable);
-      timetables.put(ScrappedNewsTVN24.getStringRepresentation(),tvn24TimeTable);
-      timetables.put(ScrappedNewsGazetaPl.getStringRepresentation(),gazetaplTimeTable);
-      timetables.put(ScrappedNewsWydarzeniaInteria.getStringRepresentation(),wydarzeniainteriaTimeTable);
+      HashMap<String, TreeMap<LocalDate, Integer>> timetablesToReturn = new HashMap<>();
+
+      timetablesToReturn.put(ScrappedNewsDGP.getStringRepresentation(), dgpTimeTable);
+      timetablesToReturn.put(ScrappedNewsPolsatNews.getStringRepresentation(), polsatnewsTimeTable);
+      timetablesToReturn.put(ScrappedNewsRMF24.getStringRepresentation(), rmf24TimeTable);
+      timetablesToReturn.put(ScrappedNewsTVN24.getStringRepresentation(), tvn24TimeTable);
+      timetablesToReturn.put(ScrappedNewsGazetaPl.getStringRepresentation(), gazetaplTimeTable);
+      timetablesToReturn.put(ScrappedNewsWydarzeniaInteria.getStringRepresentation(), wydarzeniainteriaTimeTable);
+
+      return timetablesToReturn;
    }
 
-   private TreeMap<LocalDate, Integer> generateTimeTable(String whichOne) {
+   private TreeMap<LocalDate, Integer> generateTimeTable(String whichOne, ArrayList<ScrappedNews> newsPool) {
 
-      ArrayList<ScrappedNews> chosenNewsPool = new ArrayList<>();
+      ArrayList<ScrappedNews> chosenNewsPool;
       TreeMap<LocalDate, Integer> currentTimeTable = new TreeMap<LocalDate, Integer>();
-      switch (whichOne) {
-         case "dgp":
-            chosenNewsPool.addAll(newsDGP);
-            break;
-         case "polsatnews":
-            chosenNewsPool.addAll(newsPolsatNews);
-            break;
-         case "rmf24":
-            chosenNewsPool.addAll(newsRMF24);
-            break;
-         case "tvn24":
-            chosenNewsPool.addAll(newsTVN24);
-            break;
-         case "gazetapl":
-            chosenNewsPool.addAll(newsGazetaPl);
-            break;
-         case "wydarzeniainteria":
-            chosenNewsPool.addAll(newsWydarzeniaInteria);
-            break;
-         default:
-            return currentTimeTable;
+      if (newsPool == null) {
+         chosenNewsPool = new ArrayList<>();
+
+         switch (whichOne) {
+            case "dgp":
+               chosenNewsPool.addAll(newsDGP);
+               break;
+            case "polsatnews":
+               chosenNewsPool.addAll(newsPolsatNews);
+               break;
+            case "rmf24":
+               chosenNewsPool.addAll(newsRMF24);
+               break;
+            case "tvn24":
+               chosenNewsPool.addAll(newsTVN24);
+               break;
+            case "gazetapl":
+               chosenNewsPool.addAll(newsGazetaPl);
+               break;
+            case "wydarzeniainteria":
+               chosenNewsPool.addAll(newsWydarzeniaInteria);
+               break;
+            default:
+               return currentTimeTable;
+         }
+      } else {
+         chosenNewsPool = newsPool;
       }
 
 
@@ -130,6 +141,7 @@ public class ScrappedNewsController {
          if (currentDate.isAfter(currentLatestDate)) {
             currentLatestDate = currentDate;
          }
+
          if (currentTimeTable.containsKey(currentDate)) {
             currentTimeTable.replace((currentDate), currentTimeTable.get(currentDate) + 1);
          } else {
@@ -143,11 +155,65 @@ public class ScrappedNewsController {
             }
          }
       }
-
+      System.out.println(currentTimeTable);
       return currentTimeTable;
 
 
    }
+
+   private HashMap<String, ArrayList<String>> generateTextWarningsBasedOnPreviousTimetable (String [] servicesGotten, LocalDate dateFrom, LocalDate dateTo) {
+      HashMap<String, ArrayList<String>> textWarnings = new HashMap<>();
+
+      for (String service: servicesGotten) {
+         ArrayList<String> warnings = new ArrayList<>();
+
+         TreeMap<LocalDate, Integer> currentTimeTable = timetables.get(service);
+
+         Set<Map.Entry<LocalDate, Integer>> entries = currentTimeTable.entrySet();
+
+         if (dateFrom.isBefore(currentTimeTable.firstKey())) {
+            warnings.add("Brak " + service.toUpperCase() + " przed dniem " + currentTimeTable.firstKey() );
+         }
+
+         LocalDate currentStartDate = null;
+         LocalDate currentEndDate = null;
+
+         int toJump = 0;
+
+         for (Map.Entry<LocalDate, Integer> entry : entries) {
+            if ( (entry.getKey().isAfter(dateFrom) || entry.getKey().isEqual(dateFrom) ) && entry.getKey().isBefore(dateTo)) {
+
+               if (toJump > 0) {
+                  toJump--;
+                  continue;
+               }
+
+               if (entry.getValue() <= 2) {
+                  currentStartDate = entry.getKey();
+                  currentEndDate = currentStartDate.plus(Period.ofDays(1));
+
+                  while (currentTimeTable.containsKey(currentEndDate) && currentTimeTable.get(currentEndDate) <= 2) {
+                     currentEndDate = currentEndDate.plus(Period.ofDays(1));
+                     toJump++;
+                  }
+                  if (toJump < 1) {
+                     warnings.add("Brak " + service.toUpperCase() + " w dniu " + currentStartDate);
+                  } else {
+                     warnings.add("Brak " + service.toUpperCase() + " w dniach " + currentStartDate + " -- " + currentEndDate.minus(Period.ofDays(1)));
+                  }
+               }
+            }
+         }
+         for (String s1 : warnings) {
+            System.out.println(s1);
+         }
+         textWarnings.put(service, warnings);
+      }
+      return textWarnings;
+
+      }
+
+
 
 
    @RequestMapping(path = "/timetable")
@@ -189,6 +255,7 @@ public class ScrappedNewsController {
 
       List<?> listToReturn;
       ArrayList<ScrappedNews> newsPool = new ArrayList<>();
+      String[] servicesGotten;
 
 
 //      if (LocalTime.now().isAfter(nextFetchTime)) {
@@ -199,7 +266,7 @@ public class ScrappedNewsController {
       if (service == null) {
          return new ResponseEntity<Object>("Service not specified. Available services are " + services.toString(), HttpStatus.NOT_ACCEPTABLE);
       } else {
-         String[] servicesGotten = service.split(",");
+         servicesGotten = service.split(",");
          System.out.println("Services gotten: " + Arrays.toString(servicesGotten));
          for (String singleService : servicesGotten) {
             switch (singleService) {
@@ -228,16 +295,21 @@ public class ScrappedNewsController {
          }
       }
 
+      LocalDateTime date_to = null;
+      LocalDateTime date_from = null;
+
       try {
          if (to != null) {
             to = to + " 00:00:00";
-            LocalDateTime date_to = LocalDateTime.parse(to, dtf);
-            newsPool.removeIf((sn -> sn.getDate_published().isAfter(ChronoLocalDateTime.from(date_to))));
+            date_to = LocalDateTime.parse(to, dtf);
+            LocalDateTime finalDate_to = date_to;
+            newsPool.removeIf((sn -> sn.getDate_published().isAfter(ChronoLocalDateTime.from(finalDate_to))));
          }
          if (from != null) {
             from = from + " 00:00:00";
-            LocalDateTime date_from = LocalDateTime.parse(from, dtf);
-            newsPool.removeIf((sn -> sn.getDate_published().isBefore(ChronoLocalDateTime.from(date_from))));
+            date_from = LocalDateTime.parse(from, dtf);
+            LocalDateTime finalDate_from = date_from;
+            newsPool.removeIf((sn -> sn.getDate_published().isBefore(ChronoLocalDateTime.from(finalDate_from))));
          }
       } catch (DateTimeException e) {
          return new ResponseEntity<>("Invalid date", HttpStatus.BAD_REQUEST);
@@ -254,8 +326,15 @@ public class ScrappedNewsController {
          }
       });
 
+      HashMap<String, ArrayList<String>> currentWarnings = new HashMap<>();
 
-
+      if (date_from == null) {
+         date_from =  LocalDateTime.of(2022,8,1,1,0);
+      }
+      if (date_to == null) {
+         date_to = LocalDateTime.now();
+      }
+      currentWarnings = generateTextWarningsBasedOnPreviousTimetable(servicesGotten, date_from.toLocalDate(), date_to.toLocalDate());
 
 
       if (onlyTitles != null && onlyTitles.equals("true")) {
@@ -275,26 +354,26 @@ public class ScrappedNewsController {
          newsPool.removeIf(scrappedNews -> !scrappedNews.getTitle().toLowerCase(Locale.ROOT).contains(searchFor.toLowerCase(Locale.ROOT)));
       }
 
-      HashMap <String, Integer> counts = new HashMap<>();
-      counts.put(ScrappedNewsDGP.getStringRepresentation(),0);
-      counts.put(ScrappedNewsPolsatNews.getStringRepresentation(),0);
-      counts.put(ScrappedNewsGazetaPl.getStringRepresentation(),0);
-      counts.put(ScrappedNewsTVN24.getStringRepresentation(),0);
-      counts.put(ScrappedNewsRMF24.getStringRepresentation(),0);
-      counts.put(ScrappedNewsWydarzeniaInteria.getStringRepresentation(),0);
-      for (Object o: newsPool) {
+      HashMap<String, Integer> counts = new HashMap<>();
+      counts.put(ScrappedNewsDGP.getStringRepresentation(), 0);
+      counts.put(ScrappedNewsPolsatNews.getStringRepresentation(), 0);
+      counts.put(ScrappedNewsGazetaPl.getStringRepresentation(), 0);
+      counts.put(ScrappedNewsTVN24.getStringRepresentation(), 0);
+      counts.put(ScrappedNewsRMF24.getStringRepresentation(), 0);
+      counts.put(ScrappedNewsWydarzeniaInteria.getStringRepresentation(), 0);
+      for (Object o : newsPool) {
          if (o.getClass().toString().equals(ScrappedNewsDGP.class.toString())) {
-            counts.put(ScrappedNewsDGP.getStringRepresentation(), counts.get(ScrappedNewsDGP.getStringRepresentation()) + 1 );
+            counts.put(ScrappedNewsDGP.getStringRepresentation(), counts.get(ScrappedNewsDGP.getStringRepresentation()) + 1);
          } else if (o.getClass().toString().equals(ScrappedNewsRMF24.class.toString())) {
-            counts.put(ScrappedNewsRMF24.getStringRepresentation(), counts.get(ScrappedNewsRMF24.getStringRepresentation()) + 1 );
+            counts.put(ScrappedNewsRMF24.getStringRepresentation(), counts.get(ScrappedNewsRMF24.getStringRepresentation()) + 1);
          } else if (o.getClass().toString().equals(ScrappedNewsTVN24.class.toString())) {
-            counts.put(ScrappedNewsTVN24.getStringRepresentation(), counts.get(ScrappedNewsTVN24.getStringRepresentation()) + 1 );
+            counts.put(ScrappedNewsTVN24.getStringRepresentation(), counts.get(ScrappedNewsTVN24.getStringRepresentation()) + 1);
          } else if (o.getClass().toString().equals(ScrappedNewsWydarzeniaInteria.class.toString())) {
-            counts.put(ScrappedNewsWydarzeniaInteria.getStringRepresentation(), counts.get(ScrappedNewsWydarzeniaInteria.getStringRepresentation()) + 1 );
+            counts.put(ScrappedNewsWydarzeniaInteria.getStringRepresentation(), counts.get(ScrappedNewsWydarzeniaInteria.getStringRepresentation()) + 1);
          } else if (o.getClass().toString().equals(ScrappedNewsPolsatNews.class.toString())) {
-            counts.put(ScrappedNewsPolsatNews.getStringRepresentation(), counts.get(ScrappedNewsPolsatNews.getStringRepresentation()) + 1 );
+            counts.put(ScrappedNewsPolsatNews.getStringRepresentation(), counts.get(ScrappedNewsPolsatNews.getStringRepresentation()) + 1);
          } else if (o.getClass().toString().equals(ScrappedNewsGazetaPl.class.toString())) {
-            counts.put(ScrappedNewsGazetaPl.getStringRepresentation(), counts.get(ScrappedNewsGazetaPl.getStringRepresentation()) + 1 );
+            counts.put(ScrappedNewsGazetaPl.getStringRepresentation(), counts.get(ScrappedNewsGazetaPl.getStringRepresentation()) + 1);
          } else {
             throw new Exception("error matching classes to strings");
          }
@@ -333,13 +412,12 @@ public class ScrappedNewsController {
       listToReturn = newsPool;
 
 
-
       System.out.println("returning response entity");
 
 //      listToReturn.add(counts);
 //      HttpHeaders headers = new HttpHeaders();
 //      headers.add("Access-Control-Allow-Origin", "*");
-      return new ResponseEntity<Object>(List.of(listToReturn,counts,timetables), HttpStatus.OK);
+      return new ResponseEntity<Object>(List.of(listToReturn, counts, currentWarnings), HttpStatus.OK);
 
 
 //      return new ResponseEntity<Object>(Collections.<ScrappedNews>emptyList(), HttpStatus.NOT_ACCEPTABLE);
